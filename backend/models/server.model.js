424b9dd8b91e176134284ad66884815f9b5ea0e6
Jon_menez <joanf.gimenez@gmail.com>
2022-05-22 20:14:27 -0300

    [ADD] cors, static html

--- backend/models/server.model.js
+++ backend/models/server.model.js
@@ -1,17 +1,42 @@
 const express = require('express');
-
+const cors = require('cors')
 
 class Server {
     constructor() {
-    this.app = express();
-    this.port = 3000;
-   }
+        this.app = express();
+        this.port = 3001;
 
-   listen() {
-       this.app.listen(this.port, () => {
-           console.log('Server on port', this.port);
-       })
-   }
+        //Middleware
+        this.middleware();
+
+        //Routes
+        this.routes();
+    }
+
+    middleware() {
+
+        //CORS
+        this.app.use(cors())
+
+        // Parse and lecture JSON
+        this.app.use(express.json());
+
+        //Public folder
+        this.app.use(express.static('public'));
+    }
+
+
+    routes() {
+        this.app.get('/', (req, res) => {
+            res.send('Hello World');
+        })
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Server on port', this.port);
+        })
+    }
 }
 
 module.exports = Server;
