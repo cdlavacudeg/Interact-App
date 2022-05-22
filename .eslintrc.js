bf487dd3c2bfd66f6d69568802224d076ab43554
Jon_menez <joanf.gimenez@gmail.com>
2022-05-22 20:15:40 -0300

    [ADD] node enviroment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true
     },
     "extends": [
         "eslint:recommended",
@@ -10,7 +11,8 @@ module.exports = {
     ],
     "parserOptions": {
         "ecmaFeatures": {
-            "jsx": true
+            "jsx": true,
+            "js": true
         },
         "ecmaVersion": "latest",
         "sourceType": "module"
