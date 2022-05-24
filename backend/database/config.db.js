8fade6528d9862dbf6c313a081f9790d6505f9c5
Jon_menez <joanf.gimenez@gmail.com>
2022-05-24 11:58:57 -0300

    [ADD] Database connection

---
+++ backend/database/config.db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const dbConection = async () => {
+    try {
+
+        await mongoose.connect(process.env.INTERACT_DB)
+        console.log('Database connected')
+
+    } catch (error) {
+
+        console.log(error)
+        throw new Error('Error starting database')
+
+    }
+}
+
+
+
+
+module.exports = {
+    dbConection
+}
