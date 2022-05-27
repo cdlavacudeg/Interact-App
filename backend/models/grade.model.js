ccce06a65cfd4932b1c3ea325078a21160c38d00
Carlos <cddd3871@gmail.com>
2022-05-27 12:03:12 -0400

    [UPDATE] Grade(controlls, route, model)

---
+++ backend/models/grade.model.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const gradeSchema = new Schema({
+        course: {
+            type: Schema.Types.ObjectId,
+            ref: 'Course',
+        }, 
+
+        user:  {   
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+        
+        grade: {
+            type: Number,
+            required: true
+        },
+        
+        obs: {
+            type: String,
+        }
+
+  
+}, {
+    timestamps: true
+})
+gradeSchema.methods.toJSON = function () {
+    const { __v, _id, ...grade } = this.toObject()
+    grade.uid = _id
+    return grade
+}
+
+module.exports = model('grade', gradeSchema);
--- backend/routes/classifications.route.js
+++
@@ -1,9 +0,0 @@
-
-const { Router } = require('express');
-const {classificationGet, classificationPost} = require('../controllers/classifications.controller.js')
-const router = Router();
-
-router.get('/', classificationGet )
-router.post('/', classificationPost)
-
-module.exports = router;
