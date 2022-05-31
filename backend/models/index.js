c4a98fc9db64852a61257c4007751c8edd6598e1
Jon_menez <joanf.gimenez@gmail.com>
2022-05-31 17:17:39 -0300

    [ADD] new import

--- backend/models/index.js
+++ backend/models/index.js
@@ -3,10 +3,12 @@ const User = require('../models/user.model');
 const Course = require('../models/course.model');
 const Grade = require('../models/grade.model');
 const Lesson = require('../models/lesson.model');
+const Notification = require('../models/notification.model');
 
 module.exports = {
     User,
     Course,
     Grade,
-    Lesson
+    Lesson,
+    Notification
 }
