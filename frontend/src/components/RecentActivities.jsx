947557bdca08d37bbb6f5f820a75991bb43b1819
Daniel <santoss.rdaniel@gmail.com>
2022-06-01 22:08:05 -0300

    responsive del main section del home

--- frontend/src/components/RecentActivities.jsx
+++ frontend/src/components/RecentActivities.jsx
@@ -7,27 +7,44 @@ import "@styles/recentactivities.css";
 
 function Recentactivities () {
     return (
-        <section className="recent-activities">
+        <section className="recent-activities pt-5 pb-4">
             <h2 className="recent-activities-h2">Actividad Reciente</h2>
             <div className="recent-activities-container">
-                <article className="recent-activities-item">
-                    <div className="activities-notification">1</div>
-                    <img src={biologyimg} alt="biology image" />
-                    <h1>Biologia</h1>
-                    <h2>Prof. Laura Valenzuela</h2>
-                </article>
-                <article className="recent-activities-item">
-                    <div className="activities-notification">1</div>
-                    <img src={mathimg} alt="math image" />
-                    <h1>Matematica</h1>
-                    <h2>Prof. Jorge Perez</h2>
-                </article>
-                <article className="recent-activities-item">
-                    <div className="activities-notification">1</div>
-                    <img src={geoimg} alt="geography image" />
-                    <h1>Geografia</h1>
-                    <h2>Prof. Andrea Marquez</h2>
-                </article>
+                <div className="row">
+                    <div className="col-6 col-lg-4">
+                        <article className="recent-activities-item mb-3">
+                            <div className="activities-notification">1</div>
+                            <img src={biologyimg} alt="biology image" />
+                            <div className="p-2">
+                                <h1>Biologia</h1>
+                                <h2>Prof. Laura Valenzuela</h2>
+                            </div>
+                        </article>
+                    </div>
+                    <div className="col-6 col-lg-4">
+                        <article className="recent-activities-item mb-3">
+                            <div className="activities-notification">1</div>
+                            <img src={mathimg} alt="math image" />
+                            <div className="p-2">
+                                <h1>Matematica</h1>
+                                <h2>Prof. Jorge Perez</h2>
+                            </div>
+                        </article>
+                    </div>
+                    <div className="col-6 col-lg-4">
+                        <article className="recent-activities-item mb-3">
+                            <div className="activities-notification">
+                                <span className="text-center">1</span>
+                            </div>
+                            <img src={geoimg} alt="geography image" />
+                            <div className="p-2">
+                                <h1>Geografia</h1>
+                                <h2>Prof. Andrea Marquez</h2>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+                
             </div>
         </section>
     )
