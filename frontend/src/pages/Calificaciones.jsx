3209e4cb28dfa259e61443d36f21adfa2b523c42
Daniel <santoss.rdaniel@gmail.com>
2022-06-03 19:42:53 -0300

    ADD container y component calificaciones

--- frontend/src/pages/Calificaciones.jsx
+++ frontend/src/pages/Calificaciones.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import GradesTable from '../components/GradesTable';
+import GradesTables from '../containers/Calificaciones';
+
 
 export default function Calificaciones () {
     return (
         <> <h1> Mis Calificaciones</h1>
-        <GradesTable />
+        <GradesTables />
         
         </>
     )
