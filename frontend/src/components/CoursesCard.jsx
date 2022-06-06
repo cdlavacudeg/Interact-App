bb37a982858d0a05a212bd5f9e48f4e415458c16
Jon_menez <joanf.gimenez@gmail.com>
2022-06-06 14:35:07 -0300

    [ADD] courses card component

---
+++ frontend/src/components/CoursesCard.jsx
@@ -0,0 +1,13 @@
+import '@styles/coursesCard.css'
+
+const CoursesCard = () => {
+  return (
+    <div className="courses-card">
+        <div ><img src="" alt="" /></div>
+        <h4>Materia 1</h4>
+
+    </div>
+  )
+}
+
+export default CoursesCard
