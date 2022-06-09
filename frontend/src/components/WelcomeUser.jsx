b2330f61b93dd9732cecbdcd5f5c9b474099caa6
Carlos <cddd3871@gmail.com>
2022-06-09 10:03:45 -0400

    [UPDATE] WelcomeUser and  Redux

--- frontend/src/components/WelcomeUser.jsx
+++ frontend/src/components/WelcomeUser.jsx
@@ -1,12 +1,15 @@
 import * as React from "react";
 import welcomeimg from "@img/welcome-img.svg";
 import "@styles/welcomeuser.css";
+import { useSelector } from "react-redux";
+
 
 
 function WelcomeUser() {
-    const user = JSON.parse(window.localStorage.getItem('loggedAppUser')) || "";
+    const user = useSelector((state) => state.user);
     const name = user.user.fullName
     const gender = user.user.gender
+
     return (
         <section className="welcome">
             <div className="welcome-user">
