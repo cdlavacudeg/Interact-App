eda2e4ba75a269d32705e2a053d328c7c9ccbe0b
cdlavacudeg <cristianlav10@gmail.com>
2022-06-10 12:16:13 -0500

    [UPDATE] Profile conection with redux

--- frontend/src/containers/Profile.jsx
+++ frontend/src/containers/Profile.jsx
@@ -1,14 +1,23 @@
-import * as React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import ImageNameProfile from "../components/ImgNameProfile";
 import PersonalDataProfile from "../components/PersonalDataProfile";
 import ProfesorStudentList from "../components/ProfesorStudentList";
+import { getProfile } from "../redux/actions";
 
 function Profile() {
+    const user = useSelector((state)=>state.user)
+    const profile = useSelector((state) => state.profile);
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        dispatch(getProfile(user.user.uid))
+    },[])
     return (
         <section className="profile-section">
             <ImageNameProfile />
             <PersonalDataProfile />
-            <ProfesorStudentList />
+            <ProfesorStudentList list={profile}/>
         </section>
     );
 }
