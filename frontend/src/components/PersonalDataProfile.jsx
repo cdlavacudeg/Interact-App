94810749c1e3133019b480ea8848e44324f4f352
Daniel <santoss.rdaniel@gmail.com>
2022-06-10 10:19:34 -0300

    [ADD] profile styles

--- frontend/src/components/PersonalDataProfile.jsx
+++ frontend/src/components/PersonalDataProfile.jsx
@@ -4,7 +4,7 @@ import "@styles/personaldataprofile.css";
 function PersonalDataProfile () {
     return ( 
         
-        <section className="bg-light p-2">            
+        <section className="bg-light p-2 table-section-width">            
             <div className="table-responsive table-personaldataprofile" id="no-more-tables">
                 <table className="table">
                 <thead className="thead">
