be03d0a21e9b28d19cd20c88a83ea9f86bc6c7ad
Jon_menez <joanf.gimenez@gmail.com>
2022-06-12 08:55:03 -0300

    [ADD] fuction to button redirect Home page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,17 +1,25 @@
-import React from "react";
-import '@styles/notfound.css';
+import {useNavigate} from "react-router-dom";
 import notfound from '@img/notfound.png';
 import logo from '@img/logo.png';
+import '@styles/notfound.css';
 
 const NotFound = () => {
+    const navigate = useNavigate()
+
+    const handleClick = () => navigate('/')
+
     return (
         <>
             <div className="main_404">
                 <img className="logo_404" src={logo} alt="logo" />
                 <div className="content_404">
                     <img className="img_404" src={notfound} alt="notfound" />
-                    <p className="p_404">Pagina no encontrada</p>
-                    <button className="btn_primary btn_404">Volver a la pagina principal</button>
+                    <p className="p_404">
+                        Pagina no encontrada
+                    </p>
+                    <button onClick={handleClick} className="btn_primary btn_404">
+                        Volver a la pagina principal
+                    </button>
                 </div>
 
             </div>
