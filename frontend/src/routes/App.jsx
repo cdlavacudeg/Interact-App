dbe1c06a72b8c03fbcb49bce43430bd69ed8e712
Jon_menez <joanf.gimenez@gmail.com>
2022-06-12 08:31:10 -0300

    [UPDATE] format & whitespace

--- frontend/src/routes/App.jsx
+++ frontend/src/routes/App.jsx
@@ -1,7 +1,6 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getCourses } from "../redux/actions";
 import Layout from "@containers/Layout";
@@ -10,17 +9,10 @@ import Materias from "@pages/Materias.jsx";
 import Calificaciones from "@pages/Calificaciones.jsx";
 import Perfil from "@pages/Perfil.jsx";
 import Contacto from "@pages/Contacto.jsx";
-import Biologia from "@pages/Materias/Biologia";
-import Historia from "@pages/Materias/Historia";
-import Geografia from "@pages/Materias/Geografia";
-import Informatica from "@pages/Materias/Informatica";
-import Matematicas from "@pages/Materias/Matematicas";
-import FisicoQuimica from "@pages/Materias/FisicoQuimica";
-import Ingles from "@pages/Materias/Ingles";
-import Literatura from "@pages/Materias/Literatura";
 import LoginPage from "@pages/LoginPage";
-import "@styles/App.css";
 import Admin from "../pages/Admin";
+import Materiaid from "../pages/Materiaid";
+import "@styles/App.css";
 
 const App = () => {
     const user = useSelector((state) => state.user);
@@ -41,26 +33,7 @@ const App = () => {
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
                     <Route path="/materias" element={<Materias />} />
-                    <Route path="/materias/Biologia" element={<Biologia />} />
-                    <Route path="/materias/Historia" element={<Historia />} />
-                    <Route
-                        path="/materias/Matematicas"
-                        element={<Matematicas />}
-                    />
-                    <Route
-                        path="/materias/Informatica"
-                        element={<Informatica />}
-                    />
-                    <Route path="/materias/Ingles" element={<Ingles />} />
-                    <Route
-                        path="/materias/FisicoQuimica"
-                        element={<FisicoQuimica />}
-                    />
-                    <Route
-                        path="/materias/Literatura"
-                        element={<Literatura />}
-                    />
-                    <Route path="/materias/Geografia" element={<Geografia />} />
+                    <Route path='/materias/:materiaId' element={<Materiaid />} />
                     <Route
                         path="/calificaciones"
                         element={<Calificaciones />}
