1038fd039b86e6328f2c59118371728fddf9437e
cdlavacudeg <cristianlav10@gmail.com>
2022-06-13 17:26:27 -0500

    [UPDATE] Home courses cards

--- frontend/src/components/RecentActivities.jsx
+++ frontend/src/components/RecentActivities.jsx
@@ -1,6 +1,6 @@
 import "@styles/recentactivities.css";
 import { useSelector } from "react-redux";
-
+import CoursesCard from "./CoursesCard";
 function Recentactivities() {
     const materias = useSelector((state) => state.courses);
 
@@ -8,26 +8,17 @@ function Recentactivities() {
         <section className="recent_activities pt-5 pb-4">
             <h2 className="recent_activities_h2 ">Actividad Reciente</h2>
             <div className="recent_activities_container">
-                <div className="row">
                     {materias ? (
                         materias.map(
                             (item, index) =>
                                 index < 3 && (
-                                    <div key={index} className="col-6 col-lg-4">
-                                        <article className="recent_activities_item mb-3">
-                                            <div className="activities_notification">
-                                                1
-                                            </div>
-                                            <img
-                                                src={item.image}
-                                                alt={item.courseName}
-                                            />
-                                            <div className="p-2">
-                                                <h1>{item.courseName}</h1>
-                                                <h2>{`Prof .${item.teacher.fullName}`}</h2>
-                                            </div>
-                                        </article>
-                                    </div>
+                                        <CoursesCard
+                                            name={item.courseName}
+                                            image={item.image}
+                                            nameProf={item.teacher.fullName}
+                                            key={item._id}
+                                            id={item._id}
+                                    />
                                 )
                         )
                     ) : (
@@ -35,7 +26,6 @@ function Recentactivities() {
                             <p> No tienes materias registradas</p>
                         </div>
                     )}
-                </div>
             </div>
         </section>
     );
