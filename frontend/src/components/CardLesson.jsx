ca02ec7a0184bbc57cd3750dad80a1d3e21a0120
cdlavacudeg <cristianlav10@gmail.com>
2022-06-13 16:58:24 -0500

    [UPDATE] Card Lesson accordion

--- frontend/src/components/CardLesson.jsx
+++ frontend/src/components/CardLesson.jsx
@@ -11,39 +11,52 @@ const CardLesson = () => {
 
     return (
         <>
-            {lectures.length != 0 ? (
-                lectures.map((lecture, index) => {
-                    return (
-                        <div
-                            onClick={handleVideo}
-                            className="cardLesson"
-                            key={index}
-                        >
-                            <section className="cardLesson-item">
-                                <div>
-                                    <p className="cardLesson-title">
-                                        {lecture.title}
-                                    </p>
+            <div className="accordion" id="accordionExample">
+                {lectures.length != 0 ? (
+                    lectures.map((lecture, index) => {
+                        return (
+                            <div className="accordion-item" key={index}>
+                                <h2
+                                    className="accordion-header"
+                                    id={`heading${index + 2}`}
+                                >
+                                    <button
+                                        className="accordion-button collapsed"
+                                        type="button"
+                                        data-bs-toggle="collapse"
+                                        data-bs-target={`#collapse${index + 2}`}
+                                        aria-expanded="false"
+                                        aria-controls={`collapse${index + 2}`}
+                                    >
+                                        <h3 className="accordionh3">
+                                            {" "}
+                                            {lecture.title}
+                                        </h3>
+                                    </button>
+                                </h2>
+                                <div
+                                    id={`collapse${index + 2}`}
+                                    className="accordion-collapse collapse"
+                                    aria-labelledby={`heading${index + 2}`}
+                                    data-bs-parent="#accordionExample"
+                                >
+                                    <div className="accordion-body">
+                                        <div className="cardLesson-video">
+                                            <iframe
+                                                className="cardLesson-video"
+                                                src={lecture.link}
+                                                allowFullScreen
+                                            ></iframe>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="cardLesson-img">
-                                    <img src={videoSVG} alt="video icon" />
-                                </div>
-                            </section>
-                            {video && (
-                                <div className="cardLesson-video">
-                                    <iframe
-                                        className="cardLesson-video"
-                                        src={lecture.link}
-                                        allowFullScreen
-                                    ></iframe>
-                                </div>
-                            )}
-                        </div>
-                    );
-                })
-            ) : (
-                <h5>No hay material disponible</h5>
-            )}
+                            </div>
+                        );
+                    })
+                ) : (
+                    <h5>No hay material disponible</h5>
+                )}
+            </div>
         </>
     );
 };
