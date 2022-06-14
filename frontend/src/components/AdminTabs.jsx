579376c7db6755eaf89dc75bd0f3ed24ffc46f52
Carlos <cddd3871@gmail.com>
2022-06-14 10:13:30 -0400

    [UPDATE] Admin table, style components

--- frontend/src/components/AdminTabs.jsx
+++ frontend/src/components/AdminTabs.jsx
@@ -1,20 +1,37 @@
 import { NavLink } from 'react-router-dom'
 import '@styles/AdmiTabs.css'
+import logoicon from"../assets/icons/undraw_teacher_re_sico 1.svg"
+import welcomeimg from "@img/welcome-img.svg";
+
 
 
 const AdminTabs = () => {
   return (
-      <div className="AdminId-info">
+      <div className="Admin">
           <ul className="AdminId-list">
               <li className="AdminId-item">
                   <button className="button_listAdmin">
-                      {" "}
-                      <NavLink to={"/admin/Teacher"}>Profesores</NavLink>
+                  <NavLink to={"/admin/Teacher"}>
+                      <img
+                          className="AdminId_item__button_listAdmin"
+                          src={logoicon}
+                          alt=""
+                      />
+                     Profesores
+                     </NavLink>
                   </button>
               </li>
               <li className="AdminId-item">
                   <button className="button_listAdmin">
-                      <NavLink to={"/admin/Student"}>Alumnos</NavLink>
+                  <NavLink to={"/admin/Student"}>
+                      <img
+                          className="AdminId_item__button_listAdmin"
+                          src={welcomeimg}
+                          alt=""
+                          width="400px"
+                      />
+                     Alumnos
+                     </NavLink>
                   </button>
               </li>
           </ul>
