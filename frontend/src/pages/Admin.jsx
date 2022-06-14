dcecb2cde52ae7dbf832ab699828d5de9fc839b2
cdlavacudeg <cristianlav10@gmail.com>
2022-06-14 15:11:42 -0500

    [ADD] Admin route protection

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,9 +1,25 @@
+import { useSelector } from "react-redux";
 import UserAdmin from "../containers/UserAdmin";
+import notfound from "@img/notfound.png";
+import { useNavigate } from "react-router-dom";
 
 export default function Admin() {
+    const user = useSelector(state=>state.user)
+    const navigate = useNavigate()
     return (
         <>
-            <UserAdmin />
+            {user.user.role == 'admin' ? <UserAdmin /> :(
+                <div className="content_404 unauthorized">
+                    <img className="img_404" src={notfound} alt="notfound" />
+                    <p className="p_404">Pagina no encontrada</p>
+                    <button
+                        onClick={() => navigate("/")}
+                        className="btn_primary btn_404"
+                    >
+                        Volver a la pagina principal
+                    </button>
+                </div>
+            )}
         </>
     );
 }
